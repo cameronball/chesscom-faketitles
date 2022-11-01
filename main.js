a63bfffe978cdf5995a63750d7e2bcbfa7e042d8
Cameron <49366907+cameronball@users.noreply.github.com>
2022-11-01 17:12:39 +0000

    Create main.js

---
+++ main.js
@@ -0,0 +1,3 @@
+$(document).ready(function(){
+  $('#board-layout-player-bottom').find('.myclass').find('player-component').find('player-tagline').find('user-tagline-component').prepend('<a href="https://www.chess.com/members/titled-players" target="_blank" class="user-chess-title-component">WGM</a>');
+});
