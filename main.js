b0550b3d48348496b1fe307cd76d94280057bdcb
Cameron <49366907+cameronball@users.noreply.github.com>
2022-11-01 17:27:43 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,1 @@
-$('div.board-layout-player-bottom').find('div').prepend('<a href="https://www.chess.com/members/titled-players" target="_blank" class="user-chess-title-component">WGM</a>');
+$( ".board-layout-bottom" ).find( ".user-tagline-component" ).prepend('<a href="https://www.chess.com/members/titled-players" target="_blank" class="user-chess-title-component">WGM</a>');
