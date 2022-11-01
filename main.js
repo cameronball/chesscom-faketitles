c245ad91de7d1c56cfb1b1d31aba3cb40836a912
Cameron <49366907+cameronball@users.noreply.github.com>
2022-11-01 17:17:06 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,1 @@
-$(document).ready(function(){
-  $('div.board-layout-player-bottom').find('div.player-component player-bottom').prepend('<a href="https://www.chess.com/members/titled-players" target="_blank" class="user-chess-title-component">WGM</a>');
-});
+$('div.board-layout-player-bottom').find('div').prepend('<a href="https://www.chess.com/members/titled-players" target="_blank" class="user-chess-title-component">WGM</a>');
